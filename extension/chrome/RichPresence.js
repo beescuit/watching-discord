5d514b282ae090570657bcf079a2555dc69715ec
BeeCoding <rafael@tunning.com.br>
2018-05-01 17:19:06 -0300

    Revert "(YOUTUBE) inject script so we can get data from the window object"
    This reverts commit cb4fcbead258a29168a90a006f489fc07aff5862.

--- extension/chrome/RichPresence.js
+++ extension/chrome/RichPresence.js
@@ -7,21 +7,27 @@ function changestatus(data) {
   xhttp.send(JSON.stringify(data));
 }
 
-function injectscript(url) {
-  script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.async = true;
-  script.onload = function(){
-    console.log("Loaded script");
-  };
-  script.src = url;
-  document.getElementsByTagName('head')[0].appendChild(script);
-}
-
 if (window.location.href.startsWith("https://www.youtube.com/watch?v=")) {
   // Youtube
-  // Inject script to be able to access the window object.
-  injectscript(chrome.extension.getURL('youtube.js'));
+  var video = document.getElementsByTagName("video")[0];
+  video.ontimeupdate = () => {
+    // Dumb thing to do but we don't have access to the window.ytplayer variable =(
+    var title = document.getElementsByClassName("title")[0].innerHTML.split(">")[1].split("<")[0];
+    var author = document.getElementById("owner-name").innerHTML.split(">")[1].split("<")[0];
+    // Get time and duration from HTML's api
+    var time = video.currentTime;
+    var duration = video.duration;
+    // Call the changestatus function
+    changestatus({type: "youtube", title, author, time, duration});
+  }
+
+  // Some checks to remove the presence when not watching
+  video.onpause = () => {
+    changestatus({type: "youtube/stop"});
+  }
+  video.onended = () => {
+    changestatus({type: "youtube/stop"});
+  }
 } else if (window.location.href.startsWith("https://www.netflix.com/watch/")) {
   // GAMBIARRA
   setTimeout(() => {

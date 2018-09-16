a44f3260b308ef61a2af72e7f5d1665ceda8e417
BeeCoding <rafael@tunning.com.br>
2018-09-15 22:43:23 -0300

    StandardJS

--- extension/chrome/RichPresence.js
+++ extension/chrome/RichPresence.js
@@ -1,60 +1,61 @@
 // RichPresence loader for websites
 
-function changestatus(data) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.open("POST", "http://127.0.0.1:3000/changestatus", true);
-  xhttp.setRequestHeader("Content-type", "application/json");
-  xhttp.send(JSON.stringify(data));
+function changestatus (data) {
+  var xhttp = new window.XMLHttpRequest()
+  xhttp.open('POST', 'http://127.0.0.1:3000/changestatus', true)
+  xhttp.setRequestHeader('Content-type', 'application/json')
+  xhttp.send(JSON.stringify(data))
 }
 
-if (window.location.href.startsWith("https://www.youtube.com/watch?v=")) {
+if (window.location.href.startsWith('https://www.youtube.com/watch?v=')) {
   // Youtube
-  var video = document.getElementsByTagName("video")[0];
+  var video = document.getElementsByTagName('video')[0]
   video.ontimeupdate = () => {
     // Dumb thing to do but we don't have access to the window.ytplayer variable =(
-    var title = document.getElementsByClassName("title")[0].innerHTML.split(">")[1].split("<")[0];
-    var author = document.querySelectorAll("#owner-name")[1].innerHTML.split(">")[1].split("<")[0];
+    var title = document.getElementsByClassName('title')[0].innerHTML.split('>')[1].split('<')[0]
+    var author = document.querySelectorAll('#owner-name')[1].innerHTML.split('>')[1].split('<')[0]
     // Get time and duration from HTML's api
-    var time = video.currentTime;
-    var duration = video.duration;
+    var time = video.currentTime
+    var duration = video.duration
     // Call the changestatus function
-    changestatus({type: "youtube", title, author, time, duration});
+    changestatus({type: 'youtube', title, author, time, duration})
   }
 
   // Some checks to remove the presence when not watching
   video.onpause = () => {
-    changestatus({type: "youtube/stop"});
+    changestatus({type: 'youtube/stop'})
   }
   video.onended = () => {
-    changestatus({type: "youtube/stop"});
+    changestatus({type: 'youtube/stop'})
   }
-} else if (window.location.href.startsWith("https://www.netflix.com/watch/")) {
+} else if (window.location.href.startsWith('https://www.netflix.com/watch/')) {
   // GAMBIARRA
   setTimeout(() => {
-    var video = document.getElementById(window.location.pathname.split("/")[2]).children[0];
+    var title, time, duration, desc
+    var video = document.getElementById(window.location.pathname.split('/')[2]).children[0]
     video.ontimeupdate = () => {
-      if (document.getElementsByClassName("ellipsize-text")[0].children.length > 0) {
+      if (document.getElementsByClassName('ellipsize-text')[0].children.length > 0) {
         // Series
-        var title = document.getElementsByClassName("ellipsize-text")[0].children[0].innerHTML;
-        var desc = document.getElementsByClassName("ellipsize-text")[0].children[1].innerHTML + " " + document.getElementsByClassName("ellipsize-text")[0].children[2].innerHTML;
-        var time = video.currentTime;
-        var duration = video.duration;
-        changestatus({type: "netflix", title, desc, time, duration});
+        title = document.getElementsByClassName('ellipsize-text')[0].children[0].innerHTML
+        desc = document.getElementsByClassName('ellipsize-text')[0].children[1].innerHTML + ' ' + document.getElementsByClassName('ellipsize-text')[0].children[2].innerHTML
+        time = video.currentTime
+        duration = video.duration
+        changestatus({type: 'netflix', title, desc, time, duration})
       } else {
         // Film
-        var title = document.getElementsByClassName("ellipsize-text")[0].innerHTML;
-        var time = video.currentTime;
-        var duration = video.duration;
-        changestatus({type: "netflix", title, desc: false, time, duration});
+        title = document.getElementsByClassName('ellipsize-text')[0].innerHTML
+        time = video.currentTime
+        duration = video.duration
+        changestatus({type: 'netflix', title, desc: false, time, duration})
       }
     }
 
     // Some checks to remove the presence when not watching
     video.onpause = () => {
-      changestatus({type: "netflix/stop"});
+      changestatus({type: 'netflix/stop'})
     }
     video.onended = () => {
-      changestatus({type: "netflix/stop"});
+      changestatus({type: 'netflix/stop'})
     }
   }, 10000)
 }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const port = 3000;
+const port = 3000
 
 const DRPCs = {
   youtube: require('discord-rpc'),
@@ -8,28 +8,28 @@ const DRPCs = {
 const cIds = {
   youtube: '434442633457172482',
   netflix: '435127726186430475'
-};
+}
 
-var youtube = false;
-var netflix = false;
+var youtube = false
+var netflix = false
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 const rpcs = {
   youtube: new DRPCs.youtube.Client({ transport: 'ipc' }),
-  netflix: new DRPCs.netflix.Client({ transport: 'ipc' }),
+  netflix: new DRPCs.netflix.Client({ transport: 'ipc' })
 }
 
-async function setActivity() {
+async function setActivity () {
   if (youtube) {
     if (Math.round((new Date()).getTime() / 1000) - youtube.last > 5) {
       // timed out
-      console.log("Timed out.");
-      youtube = false;
-      rpcs.youtube.clearActivity();
+      console.log('Timed out.')
+      youtube = false
+      rpcs.youtube.clearActivity()
     } else {
-      console.log("Setting activity");
+      console.log('Setting activity')
       rpcs.youtube.setActivity({
         details: youtube.title,
         state: youtube.author,
@@ -38,16 +38,16 @@ async function setActivity() {
         largeImageKey: 'youtube',
         largeImageText: 'Youtube',
         instance: false
-      });
+      })
     }
   } else if (netflix) {
     if (Math.round((new Date()).getTime() / 1000) - netflix.last > 5) {
       // timed out
-      console.log("Timed out.");
-      netflix = false;
-      rpcs.netflix.clearActivity();
+      console.log('Timed out.')
+      netflix = false
+      rpcs.netflix.clearActivity()
     } else {
-      console.log("Setting activity");
+      console.log('Setting activity')
       if (netflix.desc) {
         rpcs.netflix.setActivity({
           details: netflix.title,
@@ -57,7 +57,7 @@ async function setActivity() {
           largeImageKey: 'netflix',
           largeImageText: 'Netflix',
           instance: false
-        });
+        })
       } else {
         rpcs.netflix.setActivity({
           details: netflix.title,
@@ -66,39 +66,39 @@ async function setActivity() {
           largeImageKey: 'netflix',
           largeImageText: 'Netflix',
           instance: false
-        });
+        })
       }
     }
   }
 }
 
 rpcs.youtube.on('ready', () => {
-  console.log("Youtube RPC ready!");
+  console.log('Youtube RPC ready!')
   setInterval(() => {
-    setActivity();
-  }, 15e3);
-});
+    setActivity()
+  }, 15e3)
+})
 rpcs.netflix.on('ready', () => {
-  console.log("Netflix RPC ready!");
+  console.log('Netflix RPC ready!')
   setInterval(() => {
-    setActivity();
-  }, 15e3);
-});
+    setActivity()
+  }, 15e3)
+})
 
-app.use(express.json());
+app.use(express.json())
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 app.post('/changestatus', (req, res) => {
   if (!rpcs.youtube || !rpcs.netflix) {
-    res.send("RPC not ready!");
-    return;
+    res.send('RPC not ready!')
+    return
   }
-  if (req.body.type == "youtube") {
+  if (req.body.type === 'youtube') {
     youtube = {
       title: req.body.title,
       author: req.body.author,
@@ -106,12 +106,12 @@ app.post('/changestatus', (req, res) => {
       end: Math.round((new Date()).getTime() / 1000) + (Math.round(req.body.duration, 0) - Math.round(req.body.time, 0)),
       last: Math.round((new Date()).getTime() / 1000)
     }
-    res.send('success!');
-  } else if (req.body.type == "youtube/stop") {
-    youtube = false;
-    res.send('stopped');
-    rpcs.youtube.clearActivity();
-  } else if (req.body.type == "netflix") {
+    res.send('success!')
+  } else if (req.body.type === 'youtube/stop') {
+    youtube = false
+    res.send('stopped')
+    rpcs.youtube.clearActivity()
+  } else if (req.body.type === 'netflix') {
     netflix = {
       title: req.body.title,
       desc: req.body.desc,
@@ -119,14 +119,14 @@ app.post('/changestatus', (req, res) => {
       end: Math.round((new Date()).getTime() / 1000) + (Math.round(req.body.duration, 0) - Math.round(req.body.time, 0)),
       last: Math.round((new Date()).getTime() / 1000)
     }
-    res.send('success!');
-  } else if (req.body.type == "netflix/stop") {
-    netflix = false;
-    rpcs.netflix.clearActivity();
-    res.send('stopped');
+    res.send('success!')
+  } else if (req.body.type === 'netflix/stop') {
+    netflix = false
+    rpcs.netflix.clearActivity()
+    res.send('stopped')
   }
-});
+})
 
-app.listen(port, () => console.log(`Web server started! (${port})`));
-rpcs.youtube.login({ clientId: cIds.youtube }).catch(console.error);
-rpcs.netflix.login({ clientId: cIds.netflix }).catch(console.error);
+app.listen(port, () => console.log(`Web server started! (${port})`))
+rpcs.youtube.login({ clientId: cIds.youtube }).catch(console.error)
+rpcs.netflix.login({ clientId: cIds.netflix }).catch(console.error)
